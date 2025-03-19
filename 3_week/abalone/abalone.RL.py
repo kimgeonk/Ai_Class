b63e6bfd4b1cc26e5a86b5139b8795519c493ebe
kimgeonk <162672654+kimgeonk@users.noreply.github.com>
2025-03-19 16:12:03 +0900

    3주차과제 : 전복의 나이와 상관관계

--- 3_week/abalone/abalone.RL.py
+++ 3_week/abalone/abalone.RL.py
@@ -3,6 +3,8 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 file_path = r"C:\Users\kimge\OneDrive\문서\Desktop\김 건\가천대학교\2025년 4학년 1학기_시간표\인공지능개론\3_week\abalone\abalone.csv"
 df = pd.read_csv(file_path)
@@ -21,3 +23,15 @@ rl_model.fit(x_train, y_train)
 y_pred = rl_model.predict(x_test)
 
 print('평균제곱근오차', mean_squared_error(y_pred, y_test)) # 3.8048405502392346
+#산점도
+effect_thing = ['Length', 'Diameter', 'Height', 'Whole_weight', 'Shucked_weight', 'Viscera_weight', 'Shell_weight']
+plt.figure(figsize=(8, 6))
+for i, feature in enumerate(effect_thing):
+    plt.subplot(3, 3, i+1)
+    plt.scatter(x_test[feature], y_test, color='blue', alpha=0.6, label=f'test({feature})')
+    plt.scatter(x_test[feature], y_pred, color='yellow', alpha=0.6, label=f'pred({feature})')
+    plt.xlabel(feature)
+    plt.ylabel('Rings')
+    plt.legend()
+plt.tight_layout()
+plt.show()
